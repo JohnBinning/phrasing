b45fcd80fb54e43f2aa5e287e0e14898d61ad257
John Binning <john.binning17@gmail.com>
2018-09-13 00:29:36 -0600

    add dadJoke component

---
+++ Components/Joke.js
@@ -0,0 +1,4 @@
+// Vue.component('dad-joke', {
+//   props: ['joke'],
+//   template: '<div class="dad-joke">{{ joke.joke }}</div>'
+// })
--- index.js
+++ index.js
@@ -1,27 +1,31 @@
 
 fetch('https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_ten')
-  .then( res => res.json())
-  .then( json => {
-    app.jokes = json;
-  });
+.then( res => res.json())
+.then( json => {
+  app.jokes = json;
+});
 
+fetch(' https://icanhazdadjoke.com/', {
+  headers: {
+    "accept": "application/json"
+  }
+})
+.then( res => res.json())
+.then( json => {
+  app.dadJokes.push(json);
+  console.log(json.joke)
+});
+
+var dadJoke = Vue.component('dad-joke', {
+  props: ['joke'],
+  template: '<div class="dad-joke">{{ joke }}</div>'
+});
 
 const app = new Vue({
   el: '#app',
   data: {
-    jokes: [
-      { 
-        setup: 'setup line1',
-        punchline: 'punch line1'
-      },
-      { 
-        setup: 'setup line2',
-        punchline: 'punch line2'
-      },
-      { 
-        setup: 'setup line3',
-        punchline: 'punch line3'
-      },
-    ]
-  }
+    jokes: [],
+    dadJokes: []
+  },
+  components: {dadJoke: dadJoke},
 })
